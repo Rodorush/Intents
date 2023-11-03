e28748dc7852f3d019a4f11e908bcc521d2ecfcc
Rodolfo Pereira de Andrade <rodolfop.andrade@gmail.com>
2023-11-03 20:48:13 -0300

    Demonstrando permissões
    Além do uso de diferentes actions

--- app/src/main/java/br/edu/scl/ifsp/sdm/intents/MainActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/sdm/intents/MainActivity.kt
@@ -1,10 +1,23 @@
 package br.edu.scl.ifsp.sdm.intents
 
+import android.Manifest.permission.CALL_PHONE
 import android.content.Intent
+import android.content.Intent.ACTION_CALL
+import android.content.Intent.ACTION_CHOOSER
+import android.content.Intent.ACTION_DIAL
+import android.content.Intent.ACTION_PICK
+import android.content.Intent.ACTION_VIEW
+import android.content.Intent.EXTRA_INTENT
+import android.content.Intent.EXTRA_TITLE
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import br.edu.scl.ifsp.sdm.intents.Extras.PARAMETER_EXTRA
@@ -17,6 +30,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private lateinit var parameterArl: ActivityResultLauncher<Intent>
+    private lateinit var callPhonePermissionArl: ActivityResultLauncher<String>
+    private lateinit var pickImageArl: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +47,26 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        callPhonePermissionArl = registerForActivityResult(ActivityResultContracts.RequestPermission()) { permissionGranted ->
+            if(permissionGranted) {
+                callPhone(true)
+            }else {
+                Toast.makeText(this,
+                    getString(R.string.permission_required_to_call), Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        pickImageArl = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            with(result) {
+                if(resultCode == RESULT_OK) {
+                    data?.data?.also {
+                        activityMainBinding.parameterTv.text = it.toString()
+                        startActivity(Intent(ACTION_VIEW).apply { data = it })
+                    }
+                }
+            }
+        }
+
         activityMainBinding.apply {
             parameterBt.setOnClickListener {
                 val parameterIntent = Intent(this@MainActivity, ParameterActivity::class.java).apply {
@@ -40,6 +75,7 @@ class MainActivity : AppCompatActivity() {
                 parameterArl.launch(parameterIntent)
             }
         }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -50,25 +86,49 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.openActivityMi -> {
+                val parameterIntent = Intent("OPEN_PARAMETER_ACTIVITY_ACTION").apply {
+                    putExtra(PARAMETER_EXTRA, activityMainBinding.parameterTv.text)
+                }
+                parameterArl.launch(parameterIntent)
                 true
             }
             R.id.viewMi -> {
+                browserIntent()
+                startActivity(browserIntent())
                 true
             }
 
             R.id.callMi -> {
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if(checkSelfPermission(CALL_PHONE) == PERMISSION_GRANTED) {
+                        callPhone(true)
+                    }else {
+                        callPhonePermissionArl.launch(CALL_PHONE)
+                    }
+                }else {
+                    callPhone(true)
+                }
                 true
             }
 
             R.id.dialMi -> {
+                callPhone(false)
                 true
             }
 
             R.id.pickMi -> {
+                val imageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).path
+                pickImageArl.launch(Intent(ACTION_PICK).apply { setDataAndType(Uri.parse(imageDir), "image/*") })
                 true
             }
 
             R.id.chooserMi -> {
+                startActivity(
+                    Intent(ACTION_CHOOSER).apply {
+                        putExtra(EXTRA_TITLE, getString(R.string.choose_your_favorite_browser))
+                        putExtra(EXTRA_INTENT, browserIntent())
+                    }
+                )
                 true
             }
 
@@ -77,4 +137,14 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    private fun callPhone(call: Boolean) {
+        startActivity(Intent(if(call) ACTION_CALL else ACTION_DIAL).apply {
+            "tel:${activityMainBinding.parameterTv.text}".also {
+                data = Uri.parse(it)
+            }
+        })
+    }
+
+    private fun browserIntent(): Intent = Intent(ACTION_VIEW, Uri.parse(activityMainBinding.parameterTv.text.toString()))
 }
